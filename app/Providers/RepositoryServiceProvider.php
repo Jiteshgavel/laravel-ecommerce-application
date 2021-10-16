9b3aa3cd0edc8bc5c5247bc0f6215cb16f7d8949
Jiteshgavel <jiteshgavel000@gmail.com>
2021-10-16 15:24:36 +0530

    Products Section Part 3 complete

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -8,7 +8,8 @@ use App\Contracts\CategoryContract;
 use App\Repositories\CategoryRepository;
 use App\Contracts\BrandContract;
 use App\Repositories\BrandRepository;
-
+use App\Contracts\ProductContract;
+use App\Repositories\ProductRepository;
 class RepositoryServiceProvider extends ServiceProvider
 {
 
@@ -16,6 +17,7 @@ class RepositoryServiceProvider extends ServiceProvider
         CategoryContract::class         =>          CategoryRepository::class,
         AttributeContract::class        =>          AttributeRepository::class,
         BrandContract::class            =>          BrandRepository::class,
+        ProductContract::class          =>          ProductRepository::class,
     ];
 
 
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
-    <div class="app-sidebar__user">
+    {{-- <div class="app-sidebar__user">
         <div>
             <p class="app-sidebar__user-name">John Doe</p>
             <p class="app-sidebar__user-designation">Frontend Developer</p>
         </div>
-    </div>
+    </div> --}}
     <ul class="app-menu">
         <li>
             <a class="app-menu__item {{ Route::currentRouteName() == 'admin.dashboard' ? 'active' : '' }}" href="{{ route('admin.dashboard') }}">
